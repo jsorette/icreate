c0799ac314c13479a31de7fa62c0a75746e673cf
Johan Sorette <sorette.johan@gmail.com>
2019-03-22 11:15:11 +0100

    roots generation

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,69 @@
-colors = []
-gardens = []
+colors = [];
+gardens = [];
+
+var leafs = [];
+var bg = 0;
+var fg = 255;
+var root;
 
 setup = () => {
+    createCanvas(windowWidth, windowHeight);
+    smooth();
+    background(fg);
+    stroke(bg);
+    root = new Branch(
+        new Coord(width / 2, height),
+        null,
+        -HALF_PI,
+        100
+    )
+    var parents = [root];
+    parents.map(parent => parent.split())
+    while (leafs.length < 194) {
+        parents = leafs;
+        leafs = [];
+        parents.map(parent => parent.split())
+    }
+}
 
+draw = () => {
+    root.draw();
+}
+
+class Branch {
+    constructor(debut, parent, rotation, length) {
+        this.debut = debut;
+        this.fin = new Coord(debut.x + length * cos(rotation), debut.y + length * sin(rotation));
+        this.parent = parent;
+        this.childs = [];
+        this.rotation = rotation;
+        this.length = length;
+    }
+
+    split = () => {
+        for (var i = 0; i < 2; i++) {
+            var child = new Branch(
+                this.fin,
+                this,
+                this.rotation + random(-1, 1),
+                this.length * 1
+            )
+            this.childs.push(child);
+            leafs.push(child);
+        }
+    }
+
+    draw = () => {
+        line(this.debut.x, this.debut.y, this.fin.x, this.fin.y);
+        this.childs.forEach(child => child.draw());
+    }
+}
+
+class Coord {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 onColorsChanged = () => {
@@ -12,6 +73,4 @@ onColorsChanged = () => {
         || (garden[".Spon"] && colors.includes('blue'))
         || (garden[".Exotic"] && colors.includes('red'))
     )
-    console.log(colors);
-    console.log(gardens.length);
 }
