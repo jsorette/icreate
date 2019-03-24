a523e8159656f85c3de49033a75f21c2526f69c9
Johan Sorette <sorette.johan@gmail.com>
2019-03-24 10:36:40 +0100

    growing roots

--- public/sketch.js
+++ public/sketch.js
@@ -8,6 +8,7 @@ BRANCH_SHIFT = 2;
 MIN_BRANCH_WEIGHT = 0.1;
 MAX_BRANCH_WEIGHT = 0.5;
 MAX_FRAME_COUNT_APPEARANCE = 30;
+NB_STEPS_FULL_APPEARANCE = 5;
 
 colors = ['yellow', 'blue', 'green'];
 gardens = [];
@@ -21,7 +22,7 @@ labelledLeaves = [];
 
 setup = () => {
     createCanvas(windowWidth, windowHeight);
-    smooth();
+    smooth(10);
     stroke(0);
     onColorsChanged();
     frameRate(20);
@@ -29,34 +30,10 @@ setup = () => {
 
 draw = () => {
     clear();
-    nodes.forEach(node => {
-        if (node.isRoot)
-            return;
-        if (!node.count || !node.shiftX || !node.shiftY) {
-            node.count = random(0, 10);
-            if (random(0, 1) > 0.5) {
-                node.shiftX = randomBool(1, 10) ? () => 0 : cos;
-                node.shiftY = randomBool(1, 10) ? () => 0 : sin;
-            } else {
-                node.shiftX = randomBool(1, 10) ? () => 0 : sin;
-                node.shiftY = randomBool(1, 10) ? () => 0 : cos;
-            }
-            node.speed = node.isLeaf ? random(0, 1) : 1;
-        }
-        node.count += 0.1;
-        node.x += node.shiftX(node.count) * node.speed;
-        node.y += node.shiftY(node.count) * node.speed;
-    })
-    const now = new Date();
-    if (!lastLabelUpdate || (now - lastLabelUpdate) / 1000 > LABEL_DISPLAY_LENGTH) {
-        labelledLeaves = [];
-        for (var i = 0; i < NB_LABELS; i++)
-            labelledLeaves.push(leaves[Math.round(random(0, leaves.length - 1))]);
-            lastLabelUpdate = now;
-    }
     if (root && root.childs.length > 1) {
-        leaves.forEach(leaf => leaf.drawCurveVertex());
-        // labelledLeaves.forEach(leaf => leaf.drawLabel());
+        shiftNodes();
+        selectLabelledLeaves();
+        leaves.forEach(leaf => leaf.draw());
     }
 }
 
@@ -102,7 +79,6 @@ class Branch {
         var path = [];
         if (!this.parent) {
             path.push(this.start);
-            path.push(this.start);
         }
         else {
             path = this.parent.generatePath();
@@ -110,33 +86,103 @@ class Branch {
         }
         if (this.childs.length < 1) {
             path.push(this.end);
-            path.push(this.end);
         }
         return path;
     }
 
-    drawCurveVertex = () => {
+    draw = () => {
         this.colors.forEach(color => {
-            if (color.startingFrame > frameCount)
+            const { r, g, b, weight, startingFrame } = color;
+            if (startingFrame > frameCount)
                 return;
             beginShape();
             noFill();
-            strokeWeight(color.weight);
-            stroke(color.r, color.g, color.b);
-            color.path.forEach(point => {
-                stroke(color.r, color.g, color.b);
-                curveVertex(point.x(), point.y());
-            })
+            strokeWeight(weight);
+            stroke(r, g, b);
+            this.drawCurve(color);
             endShape();
         });
+        if (this.labelled)
+        this.drawLabel();
+    }
+
+    drawCurve = (color) => {
+        if (!color.count || !color.lastCheckPoint) {
+            color.lastCheckPoint = 2;
+            color.count = 1;
+        }
+        else if (color.count >= NB_STEPS_FULL_APPEARANCE) {
+            color.lastCheckPoint+=1;
+            color.count = 1;
+        }
+
+        const { path } = color;
+        curveVertex(path[0].x(), path[0].y());
+        for (let i = 0; i < color.lastCheckPoint && i < path.length; i++) {
+            curveVertex(path[i].x(), path[i].y());
+        }
+        const i = color.lastCheckPoint;
+        if (i >= path.length - 1) {
+            curveVertex(path[path.length - 1].x(), path[path.length - 1].y());
+        }
+        else {
+            let x, y, t = color.count / NB_STEPS_FULL_APPEARANCE / 3;
+            if (i === path.length - 1) {
+                const n = path.length;
+                x = curvePoint(path[n-4].x(), path[n-3].x(), path[n-2].x(), path[n-1].x(), t + 0.66);
+                y = curvePoint(path[n-4].y(), path[n-3].y(), path[n-2].y(), path[n-1].y(), t + 0.66);
+            }
+            else {
+                x = curvePoint(path[i-2].x(), path[i-1].x(), path[i].x(), path[i+1].x(), t + 0.66);
+                y = curvePoint(path[i-2].y(), path[i-1].y(), path[i].y(), path[i+1].y(), t + 0.66);
+            }
+            curveVertex(x, y);
+            curveVertex(x, y);
+        }
+        color.count+=1;
     }
 
     drawLabel = () => {
         fill(0);
         stroke(0);
+        strokeWeight(0.1);
         ellipse(this.end.x, this.end.y, 10, 10);
-        textAlign(CENTER,BOTTOM);
+        textAlign(CENTER, BOTTOM);
         text(this.garden.Nom + "\n" + this.garden.Ville, this.end.x, this.end.y - 10);
+    }
+}
+
+shiftNodes = () => {
+    nodes.forEach(node => {
+        if (node.isRoot)
+            return;
+        if (!node.count || !node.shiftX || !node.shiftY) {
+            node.count = random(0, 10);
+            if (random(0, 1) > 0.5) {
+                node.shiftX = randomBool(1, 10) ? () => 0 : cos;
+                node.shiftY = randomBool(1, 10) ? () => 0 : sin;
+            } else {
+                node.shiftX = randomBool(1, 10) ? () => 0 : sin;
+                node.shiftY = randomBool(1, 10) ? () => 0 : cos;
+            }
+            node.speed = node.isLeaf ? random(0, 1) : 1;
+        }
+        node.count += 0.1;
+        node.x += node.shiftX(node.count) * node.speed;
+        node.y += node.shiftY(node.count) * node.speed;
+    })
+}
+
+selectLabelledLeaves = () => {
+    const now = new Date();
+    if (!lastLabelUpdate || (now - lastLabelUpdate) / 1000 > LABEL_DISPLAY_LENGTH) {
+        labelledLeaves.forEach(leaf => leaf.labelled = false);
+        for (let i = 0; i < NB_LABELS; i++) {
+            const leaf = leaves[Math.round(random(0, leaves.length - 1))];
+            leaf.labelled = true;
+            labelledLeaves.push(leaf);
+        }
+        lastLabelUpdate = now;
     }
 }
 
@@ -163,7 +209,7 @@ generateRoots = () => {
         leaves = [];
         parents.forEach(parent => parent.split())
     }
-    
+
     leaves.forEach(leaf => {
         leaf.path = leaf.generatePath();
         leaf.end.isLeaf = true;
@@ -181,13 +227,13 @@ matchGardensToLeaves = () => {
         leaf.colors = [];
 
         if (garden.SPON && colors.includes('red'))
-            leaf.colors.push({r:255 ,g: 21,b: 17, value: garden.SPON});
+            leaf.colors.push({ r: 255, g: 21, b: 17, value: garden.SPON });
         if (garden.EXOTIC && colors.includes('blue'))
-            leaf.colors.push({r:4 ,g: 163,b: 255, value: garden.EXOTIC});
+            leaf.colors.push({ r: 4, g: 163, b: 255, value: garden.EXOTIC });
         if (garden.SERRES && colors.includes('yellow'))
-            leaf.colors.push({r:232 ,g: 145,b: 21, value: garden.SERRES});
+            leaf.colors.push({ r: 232, g: 145, b: 21, value: garden.SERRES });
         if (garden.MEDIC && colors.includes('green'))
-            leaf.colors.push({r:21 ,g: 232,b: 78, value: garden.MEDIC});
+            leaf.colors.push({ r: 21, g: 232, b: 78, value: garden.MEDIC });
         shuffle(leaf.colors);
 
         leaf.colors.forEach(color => {
